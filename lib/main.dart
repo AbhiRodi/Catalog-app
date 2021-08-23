c30a39bdb89c3df2b5c45dca4a90a7001514cb14
AbhiRodi <abhishant@gmail.com>
2021-08-23 16:29:45 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(NewProject());
+}
+
+class NewProject extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('New Project'),),
+        appBar: AppBar(
+          title: Text('New Project'),
+        ),
+      ),
+    );
+  }
+}
